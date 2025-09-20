0dc27751fe2b9d79d3970d600d52f163d766fff0
Lucas Camilo <39625516+LucasCamiloo@users.noreply.github.com>
2025-09-19 21:30:41 -0300

    Add files via upload

---
+++ src/main/java/com/tadalafarma/Tadalafarma/TadalafarmaApplication.java
@@ -0,0 +1,13 @@
+package com.tadalafarma.Tadalafarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadalafarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadalafarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadalafarma/Tadalafarma/TadalafarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadalafarma.Tadalafarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadalafarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
